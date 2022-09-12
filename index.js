07cd3f61ad61be92faf6e6f3cebfbade1d8765ac
CBruno <christian.bruno1995@gmail.com>
2022-09-11 21:51:01 -0400

    updated bootstrap formatting for final project. A readMe is still needed as well as more logic to the Inquirer questions to prevent duplicate ID's

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const init = async () => {
             isTeamComplete = true;
             console.log(employees)
             generateCards(employees)
-            fs.writeFile('index.html', generateHTML(employeeCards), (error) =>
+            fs.writeFile('./dist/index.html', generateHTML(employeeCards), (error) =>
             error ? console.error(error) : console.log('HTML file written successfully!'))
         } else {
             if (employeeType === 'engineer') {
@@ -168,45 +168,51 @@ function generateCards(arr) {
     arr.forEach(element => {
         switch (element.getRole()) {
             case "Manager":
-                employeeCards.push(`<div class="card col-4" style="width: 18rem;">
-                <div class="card-body">
-                <h5 class="card-title">${element.name}</h5>
-                <p class="card-text">${element.getRole()}</p>
-                </div>
-                <ul class="list-group list-group-flush">
-                <li class="list-group-item">${element.id}</li>
-                <li class="list-group-item"><a href = "mailto: ${element.email}">${element.email}</a></li>
-                <li class="list-group-item">${element.officeNumber}</li>
-                </ul>
-            </div>`)
+                employeeCards.push(`<div class="col">
+        <div class="card bg-primary text-white text-center fw-bold h-100">
+            <div class="card-body">
+                <h5 class="card-title text-capitalize">${element.name}</h5>
+                <p class="card-text card-subtitle fw-bold"><i class="fa-solid fa-mug-hot"></i> ${element.getRole()}</p>
+            </div>
+            <ul class="list-group list-group-flush card-text text-dark">
+                <li class="list-group-item">ID: ${element.id}</li>
+                <li class="list-group-item">Email: <a href = "mailto: ${element.email}">${element.email}</a></li>
+                <li class="list-group-item">Phone: ${element.officeNumber}</li>
+            </ul>
+        </div>
+        </div>`)
                 break;
 
             case "Engineer":
-                employeeCards.push(`<div class="card col-4" style="width: 18rem;">
-                <div class="card-body">
-                <h5 class="card-title">${element.name}</h5>
-                <p class="card-text">${element.getRole()}</p>
-                </div>
-                <ul class="list-group list-group-flush">
-                <li class="list-group-item">${element.id}</li>
-                <li class="list-group-item"><a href = "mailto: ${element.email}">${element.email}</a></li>
-                <li class="list-group-item"><a href="https://github.com/${element.github}">GitHub Profile</a></li>
-                </ul>
-                </div>`)
+                employeeCards.push(`<div class="col">
+        <div class="card bg-success text-white text-center fw-bold h-100">
+            <div class="card-body">
+                <h5 class="card-title text-capitalize">${element.name}</h5>
+                <p class="card-text card-subtitle"><i class="fa-sharp fa-solid fa-glasses"></i> ${element.getRole()}</p>
+            </div>
+            <ul class="list-group list-group-flush text-dark">
+                <li class="list-group-item">ID: ${element.id}</li>
+                <li class="list-group-item">Email: <a href = "mailto: ${element.email}">${element.email}</a></li>
+                <li class="list-group-item">GitHub: <a href="https://github.com/${element.github}">${element.github}</a></li>
+            </ul>
+        </div>
+    </div>`)
                 break;
                 
             case "Intern":
-                employeeCards.push(`<div class="card col-4" style="width: 18rem;">
-                    <div class="card-body">
-                    <h5 class="card-title">${element.name}</h5>
-                    <p class="card-text">${element.getRole()}</p>
-                    </div>
-                    <ul class="list-group list-group-flush">
-                    <li class="list-group-item">${element.id}</li>
-                    <li class="list-group-item"><a href = "mailto: ${element.email}">${element.email}</a></li>
-                    <li class="list-group-item">${element.school}</li>
-                    </ul>
-                    </div>`)
+                employeeCards.push(`<div class="col">
+        <div class="card bg-secondary text-center text-white fw-bold h-100">
+            <div class="card-body">
+                <h5 class="card-title text-capitalize">${element.name}</h5>
+                <p class="card-text card-subtitle"><i class="fa-solid fa-graduation-cap"></i> ${element.getRole()}</p>
+            </div>
+            <ul class="list-group list-group-flush text-dark">
+                <li class="list-group-item">ID ${element.id}</li>
+                <li class="list-group-item">Email: <a href = "mailto: ${element.email}">${element.email}</a></li>
+                <li class="list-group-item">School: ${element.school}</li>
+            </ul>
+        </div>
+    </div>`)
                 break;
         
         }
@@ -216,36 +222,28 @@ function generateCards(arr) {
 }
 
 function generateHTML(arr) {
-    return `
-    <!DOCTYPE html>
-    <html lang="en">
-    
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-      <title>Project Team!</title>
-    </head>
-    
-    <body>
-        
-        <header class="container-fluid bg-dark text-light mb-5 p-3">
-            <div class="d-flex align-items-center">
-            <h1>Project Team</h1>
-            </div>
-            </header>
-            <section class="container">
-                <div class="row">
-                ${arr.join('\r\n')}
-                </div>
-            </section>
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    </body>`
+    return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/f6d3bd14b4.js" crossorigin="anonymous"></script>
+    <title>Project Team!</title>
+</head>
 
-
+<body>
+    <header class="container-fluid jumbotron text-center bg-dark text-light mb-5 p-3">
+        <h1>Project Team</h1>
+    </header>
+    <section class="container">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
+            ${arr.join('\r\n')}
+        </div>
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>`
 }
 
 init();
